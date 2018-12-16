60f0877dc3020f01df7f96edd498a1ebd62374fc
Gal Klein <some@some.com>
2018-12-16 07:40:53 +0200

    first commit of uvApp

---
+++ UVapp/MainActivity.cs
@@ -0,0 +1,133 @@
+﻿using Android.App;
+
+using Android.Widget;
+using Android.OS;
+using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+using Microsoft.Band;
+using Microsoft.Band.Sensors;
+
+
+[assembly: UsesPermission(Android.Manifest.Permission.Bluetooth)]
+[assembly: UsesPermission(Microsoft.Band.BandClientManager.BindBandService)]
+
+namespace UVapp
+{
+    [Activity(Label = "UVapp", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+
+        private HttpClient httpClient;
+        private IBandClient bandClient;
+
+        TextView greeting , recommendation;
+        Random rnd = new Random();
+        int uv;
+        Button uvButton;
+        Button connectBandButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            greeting = FindViewById<TextView>(Resource.Id.greetTxt);
+            recommendation = FindViewById<TextView>(Resource.Id.recommendTxt);
+            uvButton = FindViewById<Button>(Resource.Id.getUVbtn);
+            connectBandButton = FindViewById<Button>(Resource.Id.connectbtn);
+            uvButton.Click += getUVClick;
+            connectBandButton.Click += connectBandClick;
+            
+        }
+
+        private async void connectBandClick(object sender, System.EventArgs e)
+        {
+            try
+            { 
+                IBandInfo[] pairedBands = BandClientManager.Instance.GetPairedBands();
+
+                if (pairedBands.Length < 1)
+                {
+                    recommendation.Text = "Band not found!";
+                    return;
+                }
+
+                bandClient = BandClientManager.Instance.Create(BaseContext, pairedBands[0]);
+
+                recommendation.Text = "Connecting...";
+                ConnectionState connState = await bandClient.ConnectTaskAsync();
+
+                if (connState == ConnectionState.Connected)
+                    recommendation.Text = "Connection Successful!";
+                else
+                    recommendation.Text = "Connection Failed!";
+                
+            }
+            catch (BandException ex)
+            {
+                recommendation.Text = "Error!!\n";
+                recommendation.Text += ex.Message;
+            }
+            
+        }
+
+        private void getUVClick(object sender, System.EventArgs e)
+        {
+            //try
+            //{
+                //uv = rnd.Next(0, 11);
+            if (bandClient == null)
+            {
+                recommendation.Text = "Connect band first";
+                return;
+            }
+
+            var uvSensor = bandClient.SensorManager.CreateUVSensor();
+                
+            uvSensor.ReadingChanged += (o, args) =>
+            {
+                    
+                UVIndexLevel uvi = args.SensorReading.UVIndexLevel;
+                RunOnUiThread(() => {
+                    recommendation.Text = "Getting recommendation...";
+                    // I don't know how to get a numerical 0-11 value
+                    string recString = ClientRecommendations.getEnumUVRecommendation(uvi);
+                    recommendation.Text = recString;
+                    //uvSensor.StopReadings();
+                });
+                
+            };
+
+            recommendation.Text = "Getting UV Reading...";
+            uvSensor.StartReadings();
+                
+           // }
+            //catch (BandException ex)
+           // {
+              //  recommendation.Text = ex.Message;
+           // }
+        }
+
+
+        
+        private async Task<string> getEnumUVRecommendation(UVIndexLevel uvi)
+        {
+            if (httpClient == null)
+            {
+                httpClient = new HttpClient();
+            }
+            return await ServerRecommendations.getEnumUVRecommendation(uvi, httpClient);
+        }
+
+       
+        private string getIntUVRecommendation(int uv)
+        {
+            return ClientRecommendations.getIntUVRecommendation(uv);
+        }
+   
+    }
+}
+
---
+++ UVapp/ServerRecommendations.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Microsoft.Band.Sensors;   // For UVI
+using Newtonsoft.Json;
+
+
+namespace UVapp
+{
+    class ServerRecommendations
+    {
+        private static string funcUri = "https://uvsafe.azurewebsites.net/api/HttpTrigger1?code=qa6RV0m9soe93D8mwKavYoAuVuTEIlMoAY7mH/TuSas4gs8Ge2cMnw==";
+
+        public static async Task<string> getEnumUVRecommendation(UVIndexLevel uviLevel, HttpClient client)
+        {
+            var uri = new Uri(funcUri);
+            var json = JsonConvert.SerializeObject(new { uvi = uviLevel.Name() });
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
+            HttpResponseMessage response = await client.PostAsync(uri, content);
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ToString();
+            }
+            else
+            {
+                return $"Server error: {response.StatusCode} \n {response.ReasonPhrase}";
+            }
+        }
+    }
+}
